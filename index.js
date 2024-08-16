8be2b525ca72f893aed3eac20bf2de533f8d8125
mskaushan <mskaushan@yandex.ru>
2024-08-16 04:12:26 +0300

    fix uploads

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     res.json({
         url: `/uploads/${req.file.originalname}`,
     });
+    console.log(req.file.originalname)
 });
 
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
