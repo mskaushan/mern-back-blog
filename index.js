b28e3d09d3d5f696c5f60aeb5c27da91e268452e
mskaushan <mskaushan@yandex.ru>
2024-08-16 03:05:18 +0300

    fix uploads

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/auth/register', registerValidation, handleValidationErrors, UserContr
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+app.post('/uploads', checkAuth, upload.single('image'), (req, res) => {
     res.json({
         url: `/uploads/${req.file.originalname}`,
     });
